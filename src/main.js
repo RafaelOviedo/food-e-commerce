69fb317a56569b043a82d8ef10adf1ac154e8630
Rafael Oviedo <oviedo.rp@gmail.com>
2021-10-16 19:39:13 -0300

    update(main.js): changing firebase import syntax

--- src/main.js
+++ src/main.js
@@ -2,21 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from "./store"
 import router from "./router"
+
 //import firebase package
-import firebase from 'firebase/compat/app';
+import * as firebase from "firebase/app";
 
 // define and initialize your firebase app config
-const firebaseApp = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyD264w5aW2mYuaje_8jn7sJ0LoXb9rwFm0",
   authDomain: "food-e-commerce-810f8.firebaseapp.com",
   projectId: "food-e-commerce-810f8",
   storageBucket: "food-e-commerce-810f8.appspot.com",
   messagingSenderId: "779643384521",
   appId: "1:779643384521:web:75a638ca80a522e2291318"
-});
- 
+};
+
 // Export firebaseApp
-export default firebaseApp;
+firebase.initializeApp(firebaseConfig);
   
 createApp(App)
     .use(router)
