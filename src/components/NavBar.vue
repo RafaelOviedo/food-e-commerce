739cb681badcf3137288bf8fb5c232c360f1ef98
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-24 16:41:21 -0300

    update(NavBar.vue): adding styles, logic to darkmode and dispatching actions

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,53 @@
 <template>
-    <div>
-        <nav class="navBar">
-            <div class="logo">LOGO</div>
-            <ul class="itemsBox">
-                <router-link :to="{ name: 'HomePage' }" class="listItem">Home</router-link>
-                <router-link :to="{ name: 'ProductsPage' }" class="listItem">Products</router-link>
-                <router-link :to="{ name: 'AboutPage' }" class="listItem">About</router-link>
-                <button class="cartButton">
-                    <box-icon id="cart-btn" onclick="openLateralBar()">
-                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAABl0lEQVRIS+2V8TFDQRCHv1SAClABKkAFpAJUgApQASpABaQDKqADdEAH5svcZY53ee/uIfmDnckkk9m7b3+7e7sD5mSDOXH5B88s82mqT4GTXyS/AdvAk4xZguUdAxdfwVuAH83vzfD7EjDavpbetZFTnF68C9yGPw6A675U4A7YAV6BlXhP23N6AZaB+1CbvmyztQDcAPslYGtxGBxXAQOptTRzw6B+fEeb4nXgMZAmTVFJToNfSnula3LZ+mtBraprLZbrIWncTsU6HAHngWZ9atK9mKS2kbEuxXbhc63MjH+jR7rA3hGfQ1++c8DMfbISsCm+CqfOwvMqDcLSZMtTArZWHm68xVJ6zq8E7Dkn1164wLSNB32BvU/zLQXbZMJUXWONZxQPl4L1d6DYaI7RUvsRcIQZgHUvMed0tiw1iiPI+WvKR5l1aUncRCpt7YNacDrJBBtEanET+d9k936nq+PZdJjYsWnK06Wif+tiqVWcrrncRIpLxaAMZOpsrwWrRJVxqOSyKLDznfcBl3Rzp8/fA38AfQ1IH6ifylIAAAAASUVORK5CYII="/>
-                    </box-icon>
-                    <span class="productQuantity">{{productQuantity}}</span>
-                </button>
-                <button class="toggleButton">Dark mode</button>
-            </ul>
-        </nav>
-        <router-view />
-    </div>
+  <div>
+    <nav class="navBar" :class="!darkModeBool ? 'navBarDark' : ''">
+      <div class="logo" :class="!darkModeBool ? 'darkLogo' : ''">LOGO</div>
+      <ul class="itemsBox">
+        <router-link :to="{ name: 'HomePage' }" class="listItem" :class="!darkModeBool ? 'listItemDark' : ''">Home</router-link>
+        <router-link :to="{ name: 'ProductsPage' }" class="listItem" :class="!darkModeBool ? 'listItemDark' : ''">Products</router-link>
+        <router-link :to="{ name: 'AboutPage' }" class="listItem" :class="!darkModeBool ? 'listItemDark' : ''">About</router-link>
+        <button class="cartButton" @click="openCartBox">
+            <img src="https://img.icons8.com/material-outlined/24/000000/shopping-cart--v1.png" alt="cart" v-show="darkModeBool">
+            <img src="https://img.icons8.com/material-outlined/24/ffffff/shopping-cart--v1.png" alt="cart" v-show="!darkModeBool">
+            <span class="productQuantity">{{productQuantity}}</span>
+        </button>
+          <Cart />
+        <button class="toggleButton" @click="toDarkMode" v-show="darkModeBool">Dark mode</button>
+        <button class="toggleButtonInverse" @click="toDarkModeInverse" v-show="!darkModeBool">Light mode</button>
+        <!-- <button @click="logOut">Log out</button> -->
+      </ul>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
+import 'boxicons';
+import { mapState } from 'vuex';
+import Cart from "./Cart.vue"
+
 export default {
-    name: "NavBar",
-    
-    data() {
-        return {
-            productQuantity: 0,
-        }
+  name: "NavBar",
+  components: { Cart },
+  data() {
+    return {
     }
+  },
+  methods: {
+    toDarkMode() {
+      this.$store.dispatch("toDarkMode")
+    },
+    toDarkModeInverse() {
+      this.$store.dispatch("toDarkModeInverse")
+    },
+    openCartBox() {
+      this.$store.dispatch("openCartBox")
+    }
+  },
+  computed: {
+    ...mapState(["darkModeBool"]),
+    ...mapState(["productQuantity"]),
+  }
 }
 </script>
 
@@ -38,9 +58,13 @@ export default {
     left: 5%;
     width: 90%;
     height: 8vh;
-    border-bottom: 1px solid black;
-    background: white;
+    border-bottom: 1px solid #000;
+    background: var(--main-color);
     z-index: 2;
+}
+.navBarDark {
+    background: var(--sec-color);
+    border-bottom: 1px solid #fff;
 }
 
 .logo {
@@ -48,6 +72,9 @@ export default {
     top: 30%;
     left: 20%;
     font-weight: bold;
+}
+.darkLogo {
+    color: #fff;
 }
 
 .itemsBox {
@@ -62,6 +89,9 @@ export default {
 .listItem {
     text-decoration: none;
 }
+.listItemDark {
+    color: #fff;
+}
 
 .cartButton {
     border: none;
@@ -72,7 +102,7 @@ export default {
 .productQuantity {
     position: absolute;
     top: 40%;
-    left: 67%;
+    left: 66%;
     width: 15px;
     height: 15px;
     border: 1px solid black;
@@ -82,9 +112,20 @@ export default {
 
 .toggleButton {
     width: 20%;
+    height: 35%;
     border-radius: 50px;
-    background: black;
+    background: var(--sec-color);
     color: white;
     cursor: pointer;
+    border: none;
+}
+.toggleButtonInverse {
+    width: 20%;
+    height: 35%;
+    border-radius: 50px;
+    background: #fff;
+    color: #000;
+    cursor: pointer;
+    border: none;
 }
 </style>
