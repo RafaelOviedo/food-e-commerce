018b503e47451304f146642d512b56ff8436b5a7
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-24 16:44:25 -0300

    update(store): adding different states, actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,62 @@ import axios from "axios";
 
 const store = createStore({
     state: {
-        allMeals: [],
+        allBurgers: [],
+        allPizzas: [],
+        darkModeBool: true,
+        cartBox: false,
+        productQuantity: 0,
+        cart: [],
     },
     mutations: {
-        getAllMeals(state, payload) {
-            state.allMeals = payload;
+        getAllBurgers(state, payload) {
+            state.allBurgers = payload;
+        },
+        // getAllPizzas(state, payload) {
+        //     state.allPizzas = payload;
+        // },
+        toDarkMode(state) {
+            state.darkModeBool = false;
+        },
+        toDarkModeInverse(state) {
+            state.darkModeBool = true;
+        },
+        openCartBox(state) {
+            state.cartBox = true;
+        },
+        closeCartBox(state) {
+            state.cartBox = false;
+        },
+        addToCart(state) {
+            state.productQuantity++;
         }
     },
     actions: {
-        async getAllMeals({ commit }) {
-            const response = await axios.get("./burgersAPI.json");
-            console.log("ALL MEALS FROM ACTION", response.data)
-            commit("getAllMeals", response.data);
+        async getAllBurgers({ commit }) {
+            const response = await axios.get("https://e-commerce-server-rafaeloviedo.vercel.app/api/burgers");
+            console.log("ALL BURGERS FROM ACTION", response.data)
+            commit("getAllBurgers", response.data);
+        },
+        // async getAllPizzas({ commit }) {
+        //     const response = await axios.get("https://e-commerce-server-rafaeloviedo.vercel.app/api/meals");
+        //     console.log("ALL PIZZAS FROM ACTION", response.data)
+        //     commit("getAllBurgers", response.data);
+        // },
+        toDarkMode({commit}) {
+            commit("toDarkMode")
+        },
+        toDarkModeInverse({commit}) {
+            commit("toDarkModeInverse")
+        },
+        openCartBox({commit}) {
+            commit("openCartBox")
+        },
+        closeCartBox({commit}) {
+            console.log("ENTER IN ACTION")
+            commit("closeCartBox")
+        },
+        addToCart({commit}) {
+            commit("addToCart")
         }
     },
     getters: {},
