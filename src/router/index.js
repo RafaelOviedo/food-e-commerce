b8afae8b7cca27f92c88d285cca6f0b785cced35
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-24 16:43:33 -0300

    update(router): adding components routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,14 @@ import LandingPage from '../views/LandingPage';
 import RegisterPage from '../views/RegisterPage';
 import ProductsPage from '../views/ProductsPage';
 import AboutPage from '../views/AboutPage';
+import Burgers from '../views/Burgers';
+import Pizzas from '../views/Pizzas';
+import Sushi from '../views/Sushi';
+import Arabic from '../views/Arabic';
+import BurgersDetails from '../views/BurgersDetails';
+import PizzasDetails from '../views/PizzasDetails';
+import SushiDetails from '../views/SushiDetails';
+import ArabicDetails from '../views/ArabicDetails';
 
 const routes = [
     {
@@ -36,6 +44,46 @@ const routes = [
         path: '/about',
         name: 'AboutPage',
         component: AboutPage
+    },
+    {
+        path: '/burgers',
+        name: 'Burgers',
+        component: Burgers
+    },
+    {
+        path: '/burgersDetails/:id',
+        name: 'BurgersDetails',
+        component: BurgersDetails
+    },
+    {
+        path: '/pizzas',
+        name: 'Pizzas',
+        component: Pizzas
+    },
+    {
+        path: '/pizzasDetails/:id',
+        name: 'PizzasDetails',
+        component: PizzasDetails
+    },
+    {
+        path: '/sushi',
+        name: 'Sushi',
+        component: Sushi
+    },
+    {
+        path: '/sushiDetails/:id',
+        name: 'SushiDetails',
+        component: SushiDetails
+    },
+    {
+        path: '/arabic',
+        name: 'Arabic',
+        component: Arabic
+    },
+    {
+        path: '/arabicDetails/:id',
+        name: 'ArabicDetails',
+        component: ArabicDetails
     }
 ]
 
